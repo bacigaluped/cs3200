e60d4020bcfe066405cd74427d04a58166b22046
Diego Bacigalupe <25193988+bacigaluped@users.noreply.github.com>
2019-04-03 00:56:15 -0400

    Added endpoints for various operations on the database. Links to recipes aren't working properly I think.

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,9 @@
 from app import app
 from requests import get
-from flask import render_template
+from flask import render_template, redirect, url_for
 from webargs import fields
 from webargs.flaskparser import use_kwargs
-from json import dumps
+from json import dumps, encoder
 
 api_headers={
     "X-RapidAPI-Key": "d79033f905mshb7d8af7cc775ed9p15d83ejsn0f48d10e2155"
@@ -12,7 +12,40 @@ api_headers={
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('ingredients.html')
+    return render_template(
+        'ingredients.html',
+        pantry_items=['Eggs'],
+        shopping_list=['Eggs'],
+        recipes=[{'id': 1234, 'title': 'Omelette', 'Link': '#link_to_omelette_recipe'}],
+    )
+
+
+@app.route('/add_to_shopping_list', methods=['POST'])
+@use_kwargs({'ingredient': fields.Str()})
+def add_to_shopping_list(ingredient):
+    print(f'{ingredient} needs to be added to the shopping list')
+    return redirect(url_for('index'))
+
+
+@app.route('/add_to_pantry', methods=['POST'])
+@use_kwargs({'ingredient': fields.Str()})
+def add_to_pantry(ingredient):
+    print(f'{ingredient} needs to be added to the pantry')
+    return redirect(url_for('index'))
+
+
+@app.route('/remove_from_shopping_list', methods=['POST'])
+@use_kwargs({'list_item': fields.Str()})
+def remove_from_shopping_list(list_item):
+    print(f'{list_item} would be removed from the shopping list here')
+    return redirect(url_for('index'))
+
+
+@app.route('/remove_from_pantry', methods=['POST'])
+@use_kwargs({'ingredient': fields.Str()})
+def remove_from_pantry(ingredient):
+    print(f'{ingredient} would be removed from the pantry here')
+    return redirect(url_for('index'))
 
 
 @app.route('/call_api', methods=['POST'])
@@ -22,4 +55,4 @@ def call_api(ingredients):
         headers=api_headers,
         params={'ingredients': ingredients, 'number': 5, 'ranking': 1}
     )
-    return dumps(response.json())
+    return '\n'.join([ f'{recipe["id"]} {recipe["title"]}\n' for recipe in response.json() ])
