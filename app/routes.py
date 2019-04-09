a3fb805f85ae7d1ad030c3ac9cf4e537a38d4524
Diego Bacigalupe <25193988+bacigaluped@users.noreply.github.com>
2019-04-09 06:30:07 -0400

    Almost all the buttons work as exepected and the application is working as desired.

--- app/routes.py
+++ app/routes.py
@@ -18,7 +18,8 @@ def image_url(image_extension):
     return f'https://spoonacular.com/cdn/ingredients_100x100/{image_extension}'
 
 def get_food_image(food_item):
-    response = get("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/ingredients/autocomplete",
+    response = get(
+        "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/ingredients/autocomplete",
         headers=api_headers,
         params={'query': food_item, 'number': 1}
     )
@@ -35,7 +36,7 @@ user_id = 1
 
 @app.route('/')
 @app.route('/index')
-def index():
+def index(possible_recipe_list=list()):
 
     cursor.execute(
         f'''select food_item.food_item_id, food_item, photo_url
@@ -95,7 +96,8 @@ def index():
         'ingredients.html',
         pantry_items=pantry_items,
         shopping_list=shopping_list,
-        recipes=recipes
+        recipes=recipes,
+        possible_recipes=possible_recipe_list
         # recipes=[{'id': 1234, 'title': 'Omelette', 'link': '#link_to_omelette_recipe'}]
     )
 
@@ -200,6 +202,56 @@ def remove_from_pantry(ingredient_id):
     return redirect(url_for('index'))
 
 
+def save_one_recipe_in_database(recipe_id):
+    cursor.execute(
+        f''' select *
+        from recipe
+        where recipe_id={recipe_id};'''
+    )
+
+    recipe_row = list(cursor)
+
+    if len(recipe_row) == 0:
+        response = get(
+            f'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/{recipe_id}/information',
+            headers=api_headers
+        )
+
+        recipe_info = response.json()
+
+        cursor.execute(
+            f'''insert into recipe (recipe_id, title, recipe_url, photo_url)
+            values ({recipe_id}, "{recipe_info['title']}", "{recipe_info['sourceUrl']}", "{recipe_info['image']}")'''
+        )
+
+        cnx.commit()
+
+
+def save_recipes_in_database(recipe_list):
+
+    added_recipe_list = list()
+    for recipe in recipe_list:
+        recipe_id = recipe['id']
+        save_one_recipe_in_database(recipe_id)
+        cursor.execute(
+            f''' select title, recipe_url, photo_url
+            from recipe
+            where recipe_id={recipe_id};'''
+        )
+
+        for row in cursor:
+            added_recipe_list.append(
+                {
+                    'id': recipe_id,
+                    'title': row[0],
+                    'link': row[1],
+                    'photo_link': row[2]
+                }
+            )
+
+    return added_recipe_list
+
+
 @app.route('/call_api', methods=['POST'])
 @use_kwargs({'ingredients': fields.List(fields.Str())})
 def call_api(ingredients):
@@ -207,4 +259,31 @@ def call_api(ingredients):
         headers=api_headers,
         params={'ingredients': ingredients, 'number': 5, 'ranking': 1}
     )
-    return '\n'.join([ f'{recipe["id"]} {recipe["title"]}\n' for recipe in response.json() ])
+
+    possible_recipe_list = save_recipes_in_database(response.json())
+    # TODO probably need to use a session here
+    return index(possible_recipe_list=possible_recipe_list)
+
+
+@app.route('/save_recipe', methods=['POST'])
+@use_kwargs({'recipe_id': fields.Int()})
+def save_recipe(recipe_id):
+
+    # TODO make sure this recipe isn't already saved
+    cursor.execute(
+        f'''insert into user_saved_recipe (user_id, recipe_id)
+        values ({user_id}, {recipe_id});'''
+    )
+    cnx.commit()
+    return redirect(url_for('index'))
+
+
+@app.route('/remove_recipe_from_saved', methods=['POST'])
+@use_kwargs({'recipe_id': fields.Int()})
+def remove_recipe_from_saved(recipe_id):
+    cursor.execute(
+        f'''delete from user_saved_recipe
+        where user_id={user_id} and recipe_id={recipe_id}'''
+    )
+    cnx.commit()
+    return redirect(url_for('index'))
